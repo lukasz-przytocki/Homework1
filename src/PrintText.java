fcf9b76757a9d5190e338545eae559b3399f0c8f
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-04-27 18:51:00 +0200

    Poprawka \" na początku.

--- src/PrintText.java
+++ src/PrintText.java
@@ -6,11 +6,11 @@ public class PrintText {
         System.out.println("\"Kręcąc mordą i rogami\n" +
                 "gryzła trawę wraz z jaskrami. \"\n" +
                 "\n" +
-                "„Czarna krowo, nie kręć rogiem,\n" +
+                "\"Czarna krowo, nie kręć rogiem,\n" +
                 "bo ja pieróg jem z twarogiem:\n" +
                 "gdy tak srogo rogiem kręcisz,\n" +
                 "gryźć pieroga nie mam chęci.\"\n");
-        System.out.println("„Jedz bez trwogi swe pierogi,\n" +
+        System.out.println("\"Jedz bez trwogi swe pierogi,\n" +
                 "nie są groźne krowie rogi.\n" +
                 "Jestem bardzo dobra krowa\n" +
                 "rodem z miasta Żyrardowa.\n" +
