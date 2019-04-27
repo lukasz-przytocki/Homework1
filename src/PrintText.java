b36a71f95ea0efca2ae1bdb6e9409e01af0238e2
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-04-27 18:40:37 +0200

    Dodano początek wierszyka.

--- src/PrintText.java
+++ src/PrintText.java
@@ -3,6 +3,13 @@ public class PrintText {
         System.out.println("Uczę się programowania");
         System.out.println("Uczę się programowania i commitowania");
         System.out.println("\n\"Czarna krowa w kropki bordo gryzła trawę kręcąc mordą.\"\n");
+        System.out.println("\"Kręcąc mordą i rogami\n" +
+                "gryzła trawę wraz z jaskrami. \"\n" +
+                "\n" +
+                "„Czarna krowo, nie kręć rogiem,\n" +
+                "bo ja pieróg jem z twarogiem:\n" +
+                "gdy tak srogo rogiem kręcisz,\n" +
+                "gryźć pieroga nie mam chęci.”\n");
         System.out.println("„Jedz bez trwogi swe pierogi,\n" +
                 "nie są groźne krowie rogi.\n" +
                 "Jestem bardzo dobra krowa\n" +
