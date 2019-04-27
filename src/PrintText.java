84d1790fcceba77c55feea220d8df3fd2d6497c4
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-04-27 18:19:28 +0200

    Dodanie nowej linijki komentarza.

--- src/PrintText.java
+++ src/PrintText.java
@@ -1,5 +1,6 @@
 public class PrintText {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania");
+        System.out.println("Uczę się programowania i commitowania");
     }
 }
