426a9706b8e6e321d1123553bdbcab627c933812
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-04-27 18:25:19 +0200

    Dodanie komentarza zwiazanej z usuwaniem.

--- src/PrintText.java
+++ src/PrintText.java
@@ -2,5 +2,6 @@ public class PrintText {
     public static void main(String[] args) {
         System.out.println("Uczę się programowania");
         System.out.println("Uczę się programowania i commitowania");
+        System.out.println("Uczę się programowania i commitowania i usuwania");
     }
 }
